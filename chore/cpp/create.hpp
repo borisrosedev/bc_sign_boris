58844c077e79b9af5b9a96333223e464abf083fc
borisrosedev <boris.rose.dev@gmail.com>
2026-08-19 10:10:39 +0200

    feat/pre

--- chore/cpp/create.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-enum class FileMode { MODE_WRITE, MODE_READ, MODE_EXEC };
-int createOrReadFile(std::string filename, FileMode fileMode,
-                     std::string content);
-
-int createFolder(std::string foldername);
--- chore/cpp/display.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-
-enum class MsgType { MSG_INFO, MSG_ERROR, MSG_WARNING, MSG_SUCCESS };
-
-int displayMsg(std::string content, MsgType msgType);
